389819fc245e101f6e1aec4761b91fcd7752f5ef
Denys <denys.drhn@gmail.com>
2023-03-05 11:01:34 +0200

    initial setup
    import libraries to index.js

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// const axios = require('axios/dist/node_modules/axios.cjs'); // node
+import axios from 'axios';
